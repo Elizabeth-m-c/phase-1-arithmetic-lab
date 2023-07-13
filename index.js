60050a900f68aaf19a4af10e8c6e04beeacfad06
Elizabeth-m-c <ellzabethwil937@gmail.com>
2023-07-13 23:06:38 +0300

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1=2;
+let num2=31;
+let multiply = num1 * num2;
+
+let random= Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let num3=28;
+let num4=7;
+
+let num5=20;
+let num6=8;
+
+let mod=(num5 % num6 + num6) % num6;
+mod=(mod + num6 - 4) % num6;
+
+console.log(mod)
+
+
+let set=[10,5,3,20,7,15];
+let max=Math.max(...set);
+
+console.log(max)
